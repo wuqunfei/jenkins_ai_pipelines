47aa2aaeec8f4f047c1d68c41c512ce0c44ee2d9
Qunfei Wu <qunfei.wu@allianz.com>
2022-03-01 15:28:36 +0100

    add crendentials

--- Jenkinsfile.package.groovy
+++ Jenkinsfile.package.groovy
@@ -1,42 +1,48 @@
 pipeline {
     agent any
-    stages{
-        stage('Check Condition'){
-            steps{
+    stages {
+        stage('Check Condition') {
+            steps {
                 echo 'Nexus setting up is done'
             }
         }
-        stage('Checkout Source Code'){
-            steps{
+        stage('Checkout Source Code') {
+            steps {
                 git branch: "${params.source_code_branch}", credentialsId: "${params.github_token}", url: "${params.source_code_repository_url}"
                 echo "Checkout source code done ${source_code_repository_url}"
             }
         }
-        stage('install packages'){
-            steps{
+        stage('install packages') {
+            steps {
                 sh 'make clean'
                 sh 'make install'
             }
         }
-        stage('Python lint'){
-            steps{
+        stage('Python lint') {
+            steps {
                 sh 'make lint'
             }
         }
-        stage('Test & Coverage'){
-            steps{
-                echo 'make test'
-                echo 'make coverage'
+        stage('Test & Coverage') {
+            steps {
+                sh 'make test'
+                sh 'make coverage'
             }
         }
-        stage('build package'){
-            steps{
-                echo 'make dist'
+        stage('build package') {
+            steps {
+                sh 'make dist'
             }
         }
-        stage('release package'){
-            steps{
-                echo 'make release'
+//        Setting nexus information into environment, twine can upload artifacts
+        stage('release package') {
+            environment {
+                TWINE_REPOSITORY_URL = params.nexus_py_repository_url
+            }
+            steps {
+                withCredentials([usernamePassword(credentialsId: "${params.nexus_credential}", usernameVariable: 'TWINE_USERNAME', passwordVariable: 'TWINE_PASSWORD')]) {
+                    sh 'make release'
+                }
             }
         }
     }
--- seed.package.groovy
+++ seed.package.groovy
@@ -1,27 +1,32 @@
-String github_token = "git-token-credentials"
+//github server setting
+String github_token_credential = "git-token-credentials"
 String github_host = "github.com"
 
+//central pipeline repository
 String pipeline_github_repository = "wuqunfei/jenkins_ai_pipelines"
 String pipeline_jenkins_file = "Jenkinsfile.package.groovy"
 
+//package source code
 String source_code_repository_url = "https://github.com/wuqunfei/ocr_package"
 String source_code_branch = "main"
 
+//nexus setting
 String nexus_py_repository_url = "http://localhost:8081/repository/pyrepo/"
-
-String nexus_user = "admin"
-String nexus_pwd = "admin"
+String nexus_credential= 'nexus_credential'
 
 pipelineJob("ocr-package-builder") {
+
     parameters {
-        stringParam("source_code_repository_url", source_code_repository_url, "Source Code HTTP URL")
-        stringParam("source_code_branch", source_code_branch, "Source Code Branch, default master")
-        stringParam('github_token', github_token, 'Github token credential id')
+
+        stringParam("source_code_repository_url", source_code_repository_url, "Package Source Code HTTP URL")
+        stringParam("source_code_branch", source_code_branch, "Package Source Code Branch, default main")
+        stringParam('github_token_credential', github_token_credential, 'Github token credential id')
 
         stringParam("pipeline_github_repository", pipeline_github_repository, "pipeline github project name")
         stringParam("pipeline_jenkins_file", pipeline_jenkins_file, 'pipeline file')
 
         stringParam("nexus_py_repository_url", nexus_py_repository_url,'http nexus pypi repository')
+        stringParam('nexus_credential', nexus_credential, 'neus nexus_credential id')
     }
     definition {
         cpsScm {
@@ -29,7 +34,7 @@ pipelineJob("ocr-package-builder") {
                 git {
                     remote {
                         github(pipeline_github_repository, "https", github_host)
-                        credentials(github_token)
+                        credentials(github_token_credential)
                     }
                 }
             }
