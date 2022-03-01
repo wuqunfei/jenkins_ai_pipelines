66365b1c56bc2dd2a9ed7426451a8b65b9045b94
Qunfei Wu <qunfei.wu@allianz.com>
2022-03-01 15:43:56 +0100

    update release pipeline for nexus

--- Jenkinsfile.package.groovy
+++ Jenkinsfile.package.groovy
@@ -12,28 +12,28 @@ pipeline {
                 echo "Checkout source code done ${source_code_repository_url}"
             }
         }
-//        stage('install packages') {
-//            steps {
-//                sh 'make clean'
-//                sh 'make install'
-//            }
-//        }
-//        stage('Python lint') {
-//            steps {
-//                sh 'make lint'
-//            }
-//        }
-//        stage('Test & Coverage') {
-//            steps {
-//                sh 'make test'
-//                sh 'make coverage'
-//            }
-//        }
-//        stage('build package') {
-//            steps {
-//                sh 'make dist'
-//            }
-//        }
+        stage('install packages') {
+            steps {
+                sh 'make clean'
+                sh 'make install'
+            }
+        }
+        stage('Python lint') {
+            steps {
+                sh 'make lint'
+            }
+        }
+        stage('Test & Coverage') {
+            steps {
+                sh 'make test'
+                sh 'make coverage'
+            }
+        }
+        stage('build package') {
+            steps {
+                sh 'make dist'
+            }
+        }
 //        Setting nexus information into environment, twine can upload artifacts
         stage('release package') {
             environment {
@@ -43,7 +43,7 @@ pipeline {
                 withCredentials([usernamePassword(credentialsId: "${params.nexus_credential}", usernameVariable: 'TWINE_USERNAME', passwordVariable: 'TWINE_PASSWORD')]) {
                     echo "${TWINE_REPOSITORY_URL}"
                     echo "${TWINE_USERNAME}"
-                    echo "${TWINE_PASSWORD}"
+                    sh 'make release'
                 }
             }
         }
