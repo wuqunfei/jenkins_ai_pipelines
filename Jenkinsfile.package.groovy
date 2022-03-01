01d4c213e5898978d70b1e18532bd87520f9b47b
Qunfei Wu <qunfei.wu@allianz.com>
2022-03-01 14:25:22 +0100

    add package and files

--- Jenkinsfile.package.groovy
+++ Jenkinsfile.package.groovy
@@ -1,40 +1,40 @@
 pipeline {
     agent any
     stages{
-        stage('Condition'){
+        stage('Check Condition'){
             steps{
                 echo 'Nexus setting up is done'
             }
         }
-        stage('Checkout'){
+        stage('Checkout Source Code'){
             steps{
                 git branch: "${params.source_code_branch}", credentialsId: "${params.github_token}", url: "${params.source_code_repository_url}"
                 echo "Checkout source code done ${source_code_repository_url}"
             }
         }
-        stage('install'){
+        stage('install packages'){
             steps{
                 sh 'make clean'
                 sh 'make install'
             }
         }
-        stage('lint'){
+        stage('Python lint'){
             steps{
                 sh 'make lint'
             }
         }
-        stage('Test'){
+        stage('Test & Coverage'){
             steps{
                 echo 'make test'
                 echo 'make coverage'
             }
         }
-        stage('build'){
+        stage('build package'){
             steps{
                 echo 'make dist'
             }
         }
-        stage('release'){
+        stage('release package'){
             steps{
                 echo 'make release'
             }
