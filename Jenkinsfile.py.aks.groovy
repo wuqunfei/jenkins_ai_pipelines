084b6633bdb3736c14f6f60de7461e415a2b8320
Qunfei Wu <qunfei.wu@allianz.com>
2022-03-11 12:06:19 +0100

    fix variable issue

--- Jenkinsfile.py.aks.groovy
+++ Jenkinsfile.py.aks.groovy
@@ -12,29 +12,29 @@ pipeline {
 
             }
         }
-        stage("Test Code"){
-            steps{
+        stage("Test Code") {
+            steps {
                 echo "Test code"
             }
         }
-        stage("Build Code"){
-            steps{
+        stage("Build Code") {
+            steps {
                 echo "application build done"
             }
         }
-        stage("Docker Build"){
-            steps{
+        stage("Docker Build") {
+            steps {
                 script {
                     dockerImage = docker.build("${params.application_name}:${env.BUILD_ID}")
                 }
                 echo "docker build done"
             }
         }
-        stage("Docker Publish ACR"){
-            steps{
-                script{
-                    docker_register_url =  "https://${params.acr_name}.azurecr.io"
-                    docker.withRegistry( docker_register_url, "${params.acr_credential}" ) {
+        stage("Docker Publish ACR") {
+            steps {
+                script {
+                    docker_register_url = "https://${params.acr_name}.azurecr.io"
+                    docker.withRegistry(docker_register_url, "${params.acr_credential}") {
                         dockerImage.push("${env.BUILD_ID}")
                         dockerImage.push("latest")
                     }
@@ -42,25 +42,25 @@ pipeline {
                 echo "docker push done"
             }
         }
-        stage("Kubernetes Deploy"){
-            steps{
+        stage("Kubernetes Deploy") {
+            steps {
                 withCredentials([kubeconfigContent(credentialsId: "${params.aks_kubeconfig_file_credential}", variable: 'kubeconfig_file')]) {
-                    dir ('~/.kube') {
-                        writeFile file:'config', text: "$kubeconfig_file"
+                    dir('~/.kube') {
+                        writeFile file: 'config', text: "$kubeconfig_file"
+                        sh "kubectl version"
+                        sh "helm version"
+                        sh "helm upgrade -- install ${params.application_name} ./helm -n ${params.application_namespace} " +
+                                "set image.repository=${params.acr_name}.azurecr.io/${params.application_name} " +
+                                "set image.tag= ${env.BUILD_ID}" +
+                                "set ingress.enable=true" +
+                                "set ingress.hostname=${params.application_name}.${params.subscription_zone}"
                     }
-                    sh "kubectl version"
-                    sh "helm version"
-                    sh "helm upgrade -- install ${params.application_name} ./helm -n ${params.application_namespace} " +
-                            "set image.repository=${params.acr_name}.azurecr.io/${params.application_name} "+
-                            "set image.tag= ${env.BUILD_ID}" +
-                            "set ingress.enable=true" +
-                            "set ingress.hostname=${params.application_name}.${params.subscription_zone}"
                     echo "K8s deploy is done"
                 }
             }
         }
-        stage("Service Health Check"){
-            steps{
+        stage("Service Health Check") {
+            steps {
                 echo "Service is up"
             }
         }
