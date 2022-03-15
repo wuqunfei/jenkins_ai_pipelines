4441dbae9854cd93e422bbb0ace98d1ad86c1eb4
Qunfei Wu <qunfei.wu@allianz.com>
2022-03-15 12:30:54 +0100

    add files

--- Jenkinsfile.py.aks.groovy
+++ Jenkinsfile.py.aks.groovy
@@ -47,20 +47,18 @@ pipeline {
                 withCredentials([kubeconfigContent(credentialsId: "${params.aks_kubeconfig_file_credential}", variable: 'kubeconfig_file')]) {
                     dir('~/.kube') {
                         writeFile file: 'config', text: "$kubeconfig_file"
-
                     }
                     dir('~/') {
-                        echo "helm upgrade --install ${params.application_name} ./helm \\" +
-                                "--set nameOverride=pysimple \\"
-                                "--set image.repository=${params.acr_name}.azurecr.io/${params.application_name} \\" +
-                                "--set image.tag=${env.BUILD_ID} \\" +
-                                "--set ingress.enable=true \\" +
+                        echo "helm upgrade --install ${params.application_name} ./helm " +
+                                "--set nameOverride=${params.application_name} " +
+                                "--set image.repository=${params.acr_name}.azurecr.io/${params.application_name} " +
+                                "--set image.tag=${env.BUILD_ID} " +
+                                "--set ingress.enable=true " +
                                 "--set \"ingress.hosts[0].host=${params.application_name}.${params.subscription_zone}," +
                                 "ingress.hosts[0].paths[0].path=/," +
-                                "ingress.hosts[0].paths[0].pathType=ImplementationSpecific\" \\" +
-                                "--namespace ${params.application_namespace} \\" +
+                                "ingress.hosts[0].paths[0].pathType=ImplementationSpecific\" " +
+                                "--namespace ${params.application_namespace} " +
                                 "--kubeconfig ~/.kube/config"
-
                     }
                     echo "K8s deploy is done"
                 }
