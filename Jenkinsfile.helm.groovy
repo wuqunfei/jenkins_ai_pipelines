0a529f9a9715ad15d48a28a25650dd7d04f394fb
Qunfei Wu <wu.qunfei@gmail.com>
2022-05-11 19:05:28 +0200

    Update Jenkinsfile.helm.groovy

--- Jenkinsfile.helm.groovy
+++ Jenkinsfile.helm.groovy
@@ -25,13 +25,13 @@ pipeline {
         }
         stage('helm push') {
 //             when { tag "release-*" }
-//             environment {
-//                 tag = gitTagName()
-//             }
+            environment {
+                tag = gitTagName()
+            }
             steps {
-                echo "helm is pushing to helm repository: ${parames.source_code_branch}"
+                echo "helm is pushing to helm repository:"
                 //sh "helm push ${params.helm_package_folder}/$GIT_COMMIT.take(7)/tgz.oci oci://${parames.helm_repository} --kubeconfig ${KUBE_CONFIG_PATH}"
-//                 echo "helm pushed new version ${tag}"
+                echo "helm pushed new version ${tag}"
             }
         }
     }
