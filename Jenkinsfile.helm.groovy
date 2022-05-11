00284bfb836d83e07cf9f1325ea81c0b878551c5
Qunfei Wu <wu.qunfei@gmail.com>
2022-05-11 19:12:32 +0200

    Update Jenkinsfile.helm.groovy

--- Jenkinsfile.helm.groovy
+++ Jenkinsfile.helm.groovy
@@ -30,7 +30,7 @@ pipeline {
             }
             steps {
                 echo "helm is pushing to helm repository:"
-                sh "helm push ${parames.helm_char_name}-${tag}.tgz oci://${parames.helm_repository} --kubeconfig ${KUBE_CONFIG_PATH}"
+                sh "helm push ${parames.helm_chart_name}-${tag}.tgz oci://${parames.helm_repository} --kubeconfig ${KUBE_CONFIG_PATH}"
                 echo "helm pushed new version ${tag}"
             }
         }
